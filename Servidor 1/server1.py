8d186187c4417ab3cbc391cceb8e7a2e61764391
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-10-03 14:44:43 -0600

    docs: commets

--- Servidor 1/server1.py
+++ Servidor 1/server1.py
@@ -3,10 +3,12 @@ from bson import json_util
 import requests
 
 app = Flask(__name__)
+ipA = 'http://18.220.134.88:5000'
+ipB = 'http://18.220.134.88:5000'
 
 @app.route('/notes', methods=['GET'])
 def getNotes():
-    x = requests.get('http://18.220.134.88:5000/notes')
+    x = requests.get(ipA + '/notes')            #IP SERVER A
     data = x.json()
     return data
 
@@ -17,21 +19,21 @@ def getRAM():
     return jsonify({'resources_A':resA, 'resources_B':resB})
 
 def getResourcesServerA():
-    x = requests.get('http://18.220.134.88:5000/getram')
+    x = requests.get(ipA + '/getram')           #IP SERVER A
     data = x.json()
     tmp = data['lineas'][1].split(':')
     ram = tmp[1]
-    x = requests.get('http://18.220.134.88:5000/notes')
+    x = requests.get(ipA + '/notes')            #IP SERVER A
     data = x.json()
     resA = {'RAM': int(ram[0:len(ram)-1]), 'Len': len(data['notes']), 'cpu': 4}
     return resA
 
 def getResourcesServerB():
-    x = requests.get('http://18.220.134.88:5000/getram')
+    x = requests.get(ipB + '/getram')           #IP SERVER B
     data = x.json()
     tmp = data['lineas'][1].split(':')
     ram = tmp[1]
-    x = requests.get('http://18.220.134.88:5000/notes')
+    x = requests.get(ipB + '/notes')            #IP SERVER B
     data = x.json()
     resB = {'RAM': int(ram[0:len(ram)-1]), 'Len': len(data['notes']), 'cpu': 2}
     return resB
@@ -39,9 +41,9 @@ def getResourcesServerB():
 @app.route('/addnote', methods=['POST'])
 def addNote():
     #before make the request, verify wich server to select on your resources
-
+    
     json_ = request.json
-    x = requests.post(url='http://18.220.134.88:5000/addnote', json=json_)
+    x = requests.post(url=ipA + '/addnote', json=json_)             #IP SERVER WINNER
     respon = x.json()
     return respon
 
