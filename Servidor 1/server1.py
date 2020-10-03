2391c6748377515109b9b680974a25f8d1682590
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-10-03 14:37:05 -0600

    feat: get server's resources

--- Servidor 1/server1.py
+++ Servidor 1/server1.py
@@ -6,26 +6,45 @@ app = Flask(__name__)
 
 @app.route('/notes', methods=['GET'])
 def getNotes():
-    x = requests.get('http://3.21.163.239:5000/notes')
+    x = requests.get('http://18.220.134.88:5000/notes')
     data = x.json()
     return data
 
-@app.route('/getram', methods=['GET'])
-def getRAM():
-    return "RAM: 22%"
+@app.route('/getres', methods=['GET'])
+def getRAM():    
+    resA = getResourcesServerA()
+    resB = getResourcesServerB()
+    return jsonify({'resources_A':resA, 'resources_B':resB})
 
-@app.route('/getcpu', methods=['GET'])
-def getCPU():
-    return "CPU: 12%"
+def getResourcesServerA():
+    x = requests.get('http://18.220.134.88:5000/getram')
+    data = x.json()
+    tmp = data['lineas'][1].split(':')
+    ram = tmp[1]
+    x = requests.get('http://18.220.134.88:5000/notes')
+    data = x.json()
+    resA = {'RAM': int(ram[0:len(ram)-1]), 'Len': len(data['notes']), 'cpu': 4}
+    return resA
+
+def getResourcesServerB():
+    x = requests.get('http://18.220.134.88:5000/getram')
+    data = x.json()
+    tmp = data['lineas'][1].split(':')
+    ram = tmp[1]
+    x = requests.get('http://18.220.134.88:5000/notes')
+    data = x.json()
+    resB = {'RAM': int(ram[0:len(ram)-1]), 'Len': len(data['notes']), 'cpu': 2}
+    return resB
 
 @app.route('/addnote', methods=['POST'])
 def addNote():
     #before make the request, verify wich server to select on your resources
+
     json_ = request.json
-    x = requests.post(url='http://3.21.163.239:5000/addnote', json=json_)
+    x = requests.post(url='http://18.220.134.88:5000/addnote', json=json_)
     respon = x.json()
     return respon
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001)      #correra en el puerto 5001
